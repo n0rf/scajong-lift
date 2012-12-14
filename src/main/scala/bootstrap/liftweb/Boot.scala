7dbad2b4fba3ae89a08e07744a5c7aef35ec00c9
n0rf <andi_snoopy@web.de>
2012-12-14 13:58:21 +0100

    only allow index.html and set title of the page by menu from site map

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -20,20 +20,8 @@ class Boot {
     // where to search snippet
     LiftRules.addToPackages("code")
 
-    LiftRules.jsArtifacts = net.liftweb.http.js.jquery.JQueryArtifacts
-
-    //Show the spinny image when an Ajax call starts
-    LiftRules.ajaxStart =
-      Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
-    
-    // Make the spinny image go away when it ends
-    LiftRules.ajaxEnd =
-      Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
-
     // Force the request to be UTF-8
     LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
-
-    //LiftRules.dispatch.append(RootController)
 
     // Use HTML5 for rendering
     LiftRules.htmlProperties.default.set((r: Req) =>
@@ -41,9 +29,9 @@ class Boot {
 
     // Build SiteMap
     def sitemap(): SiteMap = SiteMap(
-      Menu("Scajong - New Game") / "index",
-      Menu("Scajong") / "game"
+      Menu("Scajong - Mahjong in scala-lift") / "index"
     )
+    LiftRules.setSiteMap(sitemap)
     
     //Init the jQuery module, see http://liftweb.net/jquery for more information.
     JQueryModule.InitParam.JQuery=JQueryModule.JQuery172
