bd519bdb62f3a512c7a488abad0b29cb61029a0c
n0rf <andi_snoopy@web.de>
2012-12-14 13:59:28 +0100

    fixed warnings at case classes definitions

--- src/main/scala/code/lib/ViewNotifications.scala
+++ src/main/scala/code/lib/ViewNotifications.scala
@@ -4,8 +4,8 @@ import scajong.util.SimpleNotification
 import scajong.model.Setup
 
 // internal notifications for tiles renderer
-case class ShowCreateGameMenuNotification extends SimpleNotification
-case class ShowScoresMenuNotification extends SimpleNotification
+case class ShowCreateGameMenuNotification() extends SimpleNotification
+case class ShowScoresMenuNotification() extends SimpleNotification
 case class ShowScoresNotification(val setup:Setup) extends SimpleNotification
 
 // internal notification used to add a new score entry and send a notification about this entry to the controller
