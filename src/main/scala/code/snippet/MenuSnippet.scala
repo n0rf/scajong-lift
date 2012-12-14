8cabb01ce68cddb4f98920b9024338b1c53bed8c
n0rf <andi_snoopy@web.de>
2012-12-14 13:30:46 +0100

    use ! method to send informations to ScajongServer and handle add score entry

--- src/main/scala/code/snippet/MenuSnippet.scala
+++ src/main/scala/code/snippet/MenuSnippet.scala
@@ -12,28 +12,36 @@ import code.comet.TilesRenderer
 import code.comet.ScajongServer
 import scajong.view.HintNotification
 import scajong.view.MoveablesNotification
+import net.liftweb.http.js.JsCmds.SetValById
+import scajong.model.ScoreEntry
+import scajong.view.ShowCreateGameMenuNotification
+import scajong.view.AddNewScoreEntryNotification
+import scajong.view.ShowScoresMenuNotification
 
 class MenuSnippet extends View {
 
   Scajong.controller.attachView(this)
 
   override def autoClose = true
-  
+
   def createGame = "#createButton [onclick]" #> SHtml.ajaxInvoke(() => {
-    println("createGame")
-    ScajongServer.showCreateGameMenu
+    ScajongServer ! new ShowCreateGameMenuNotification
   })
-  
+
   def showScoresMenu = "#showScoresMenuButton [onclick]" #> SHtml.ajaxInvoke(() => {
-    println("showScoresMenu")
-    ScajongServer.showScoresMenu
+    ScajongServer ! new ShowScoresMenuNotification
   })
-  
+
   def showMoveables = "#moveablesButton [onclick]" #> SHtml.ajaxInvoke(() => {
     sendNotification(new MoveablesNotification)
   })
-  
+
   def showHint = "#hintButton [onclick]" #> SHtml.ajaxInvoke(() => {
     sendNotification(new HintNotification)
+  })
+
+  def addScoreEntry = SHtml.onSubmit(name => {
+    ScajongServer ! new AddNewScoreEntryNotification(name)
+    SetValById("entry_name", "")
   })
 }
