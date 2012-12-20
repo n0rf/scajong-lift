439a06b33c56cd43ba6282d9eaf976e708f4ad6e
n0rf <andi_snoopy@web.de>
2012-12-20 12:34:27 +0100

    new notifications for view server handling

--- src/main/scala/code/lib/ViewNotifications.scala
+++ src/main/scala/code/lib/ViewNotifications.scala
@@ -2,14 +2,20 @@ package main.scala.code.lib
 
 import scajong.util.SimpleNotification
 import scajong.model.Setup
+import scajong.model.Tile
 
 // internal notifications for tiles renderer
 case class ShowCreateGameMenuNotification() extends SimpleNotification
 case class ShowScoresMenuNotification() extends SimpleNotification
 case class ShowScoresNotification(val setup:Setup) extends SimpleNotification
+case class BackToGameNotification() extends SimpleNotification
+
+// internal notifications handled in view server
 case class RequestHintNotification() extends SimpleNotification
 case class RequestMoveablesNotification() extends SimpleNotification
+case class StartNewGameNotification(setup:Setup) extends SimpleNotification
+case class TileClickNotification(tile:Tile) extends SimpleNotification
+case class AddScoreNotification(setup:Setup, name:String, ms:Int) extends SimpleNotification
 
 // internal notification used to add a new score entry and send a notification about this entry to the controller
 case class AddNewScoreEntryNotification(val name:String) extends SimpleNotification
-case class BackToGameNotification() extends SimpleNotification
